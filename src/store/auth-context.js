d174e64da361e8f4a7f5794c3636a09187f03a0d
vikash sirvi <Vikashsirvi9999@gmail.com>
2021-10-14 19:57:26 +0530

    Building and using central Context provider

--- src/store/auth-context.js
+++ src/store/auth-context.js
@@ -1,7 +1,39 @@
 import react from "react";
+import { useEffect, useState } from "react/cjs/react.development";
 
 const AuthContext = react.createContext({
   isLoggedIn: false,
+  onLogout: () => {},
+  onLogin: () => {},
 });
+
+export const AuthContextProvider = (props) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  useEffect(() => {
+    const storedUserLoggedInInformation = localStorage.getItem("isLoggedIn");
+    if (storedUserLoggedInInformation === "1") {
+      setIsLoggedIn(true);
+    }
+  }, []);
+  const logoutHandler = () => {
+    localStorage.removeItem("isLoggedIn");
+    setIsLoggedIn(false);
+  };
+  const loginHandler = () => {
+    localStorage.setItem("isLoggedIn", "1");
+    setIsLoggedIn(true);
+  };
+  return (
+    <AuthContext.Provider
+      value={{
+        isLoggedIn: isLoggedIn,
+        onLogout: logoutHandler,
+        onLogin: loginHandler,
+      }}
+    >
+      {props.children}
+    </AuthContext.Provider>
+  );
+};
 
 export default AuthContext;
